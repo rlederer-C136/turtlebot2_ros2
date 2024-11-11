a706cdad224f8ee13b4c55a8701183f5dbb349d6
rlederer-C136 <rodney@veritascognitum.com>
2024-11-10 20:09:59 -0800

    Remove unnecessary Realsense options

--- turtlebot2_bringup/launch/turtlebot2_bringup.launch.py
+++ turtlebot2_bringup/launch/turtlebot2_bringup.launch.py
@@ -112,15 +112,6 @@ def generate_launch_description():
         launch_arguments={
         'pointcloud.enable': 'true',
         'align_depth.enable': 'true',  # Example of another parameter
-        'enable_sync': 'true',
-        'filters': 'pointcloud',
-        'initial_reset': 'true'
-        'color_width': '1280',
-        'color_height': '720',
-        'color_fps': '30',
-        'depth_width': '1280',
-        'depth_height': '720',
-        'depth_fps': '30'
     }.items()
     )
 
